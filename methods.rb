827e2d116042b3c9c2fbe69d970bcb8399aa0e6e
mark mwendwa <mwendwamark310@gmail.com>
2023-05-14 19:59:40 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,19 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+def greet (name)
+    puts "Hello,#{name}"
+
+def greet_with_default(name="programmer")
+    puts "Hello,#{name}!"    
+
+def add (num1,num2);
+    return num1 + num2 ;
+    
+def halve (number)    
+    if  number.class !== Integer
+        return nil
+
+        return number/2
